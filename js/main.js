e8847af442b7535d622b1f8e69694119192236a9
Ilya <31817995+Makasinov@users.noreply.github.com>
2018-11-13 02:04:49 +0300

    Меню покупки доработано и забирает данные с сервера

--- js/main.js
+++ js/main.js
@@ -1,53 +1,102 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET','/start_page');
-xhr.send();
-xhr.onreadystatechange = () => {
-    if (xhr.readyState != 4) return;
-    if (xhr.status != 200) {
-        return;
+
+function loadCatalogue() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/start_page');
+    xhr.send();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState != 4) return;
+        if (xhr.status != 200) {
+            return;
+        }
+        var str = xhr.responseText;
+        var count = 0;
+        var json = JSON.parse(str, (key, value) => {
+            count++;
+            return value;
+        });
+        if (count > 10) count = 10;
+        for (var i = 0; i < count; i++)
+            document.getElementById('content').innerHTML += getHTMLBlock(json[i]);
+        document.getElementById('content').innerHTML += '</div>';
     }
-    var str = xhr.responseText;
-    var count = 0;
-    var json = JSON.parse(str, (key, value) => {
-        //if (key == 'name') return value;
-        count++;
-        return value;
-    });
-    if (count > 10) count = 10;
-    for (var i = 0; i < count; i++)
-        document.getElementById('content').innerHTML += getHTMLBlock(json[i]);
-    document.getElementById('content').innerHTML += '</div>';
 }
 
 function dialogueWindow(id) {
     var dialogue = document.getElementById('dialogue');
-    if  (dialogue.style.display == "block")
-         dialogue.style.display = "none";
-    else
-    {
-        dialogue.style.display = "block";
-        var a_name = document.getElementById('name');
-        a_name.innerHTML = id;
+    if (dialogue.style.display == "block")
+        dialogue.style.display = "none";
+    else {
+        var xhr = new XMLHttpRequest();
+        var url = '/get/' + id;
+        xhr.open('GET', url);
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState != 4) return;
+            if (xhr.status != 200) {
+                return;
+            }
+            var json = JSON.parse(xhr.responseText);
+            dialogue.style.display = "block";
+            var a_name = document.getElementById('name');
+            a_name.innerHTML = json.name;
+            var a_number = document.getElementById('number');
+            a_number.innerHTML = 'В наличии: ' + json.number;
+            var a_price = document.getElementById('price');
+            a_price.innerHTML = ' x ' + json.price + 'р';
+            var a_final_price = document.getElementById('final_price');
+            a_final_price.innerHTML = ` = ${json.price}p`;
+            var a_id = document.getElementById('id');
+            a_id.innerHTML = id;
+        }
+        xhr.send();
     }
 }
 
 function getHTMLBlock(obj) {
     var string =
         '<div class="container noselect">' +
-            '<div class="title">' +
-                '<a class="name">' +
-                    obj.name + 
-                '</a>' +
-                `<a class="jlink" onClick="dialogueWindow(\'${obj._id}\')" style="width: 360px">` +
-                '<span class="b">' +
-                    obj.price +
-                'р</span>' +
-                '<span class="c">Купить</span>' + 
-                '</a>' + 
-            '</div>' +
-            '<img src="' +
-                obj.img + 
-            '" width="300" height="300"></img>' +
+        '<div class="title">' +
+        '<a class="name">' +
+        obj.name +
+        '</a>' +
+        `<a class="jlink" onClick="dialogueWindow(\'${obj._id}\')" style="width: 360px">` +
+        '<span class="b">' +
+        obj.price +
+        'р</span>' +
+        '<span class="c">Купить</span>' +
+        '</a>' +
+        '</div>' +
+        '<img src="' +
+        obj.img +
+        '" width="300" height="300"></img>' +
         '</div>';
-        return string;
+    return string;
+}
+
+function buy() {
+    var xhr = new XMLHttpRequest();
+    const id = document.getElementById('id').innerHTML;
+    var name = document.getElementById('name').innerHTML;
+    const inputNumber = document.getElementById('inputNumber').value;
+    const user = document.getElementById('user').value;
+    const tel = document.getElementById('tel').value;
+    const email = document.getElementById('email').value;
+
+    var url2 =  
+        `/buy?id=${id}&
+        name=${name}&
+        inputNumber=${inputNumber}&
+        user=${user}&
+        tel=${tel}&
+        email=${email}`;
+    xhr.open('GET', url2);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState != 4) return;
+        if (xhr.status != 200) {
+            return;
+        }
+        // if (xhr.responseText == 'success')
+        //     alert('Вы успешно заказали товар!');
+        alert(xhr.responseText);
+    }
+    xhr.send();
 }
--- server.js
+++ server.js
@@ -4,52 +4,79 @@ const assert = require('assert');
 // Connection URL
 const url = 'mongodb://localhost:27017';
 
-// Database Name
-const dbName = 'myDB';
-
 const express = require('express');
 const app = express();
 
+const buy_url = '/buy?';
+const get_url = '/get/';
+
+
 app.use(express.static(__dirname));
 
-app.get('/start_page',(req,res) => {
-    console.log('New connection....');
-    const client = new MongoClient(url, {useNewUrlParser: true});
-    client.connect(function(err) {
+app.get('/buy*', (req, res) => {
+        var str = req.url.substr(buy_url.length, req.url.length);
+        str = str.split('&');
+        for (var i = 0; i < str.length; i++)
+            str[i] = str[i].substr(str[i].indexOf('=') + 1, str[i].length);
+        console.log(str);
+        res.send(str);
+});
+
+app.get('/get/*', (req, res) => {
+    if (req.url.indexOf(get_url) == 0) {
+        var responde = '';
+        var id = req.url.substr(get_url.length, req.url.length);
+        // console.log('--> ' + get_url + id);
+        const client = new MongoClient(url, { useNewUrlParser: true });
+
+        client.connect(function (err) {
+            assert.equal(null, err);
+            // console.log('Есть соединение');
+            const db = client.db('myDB');
+            db.collection('Store').find(
+                {}, { projection: { _id: 1, name: 1, price: 1, number: 1, img: 1 } }).toArray(
+                    (err, result) => {
+                        if (err) throw err;
+                        // console.log('Нет ошибок');
+                        for (var i = 0; i < result.length; i++) {
+                            if (result[i]._id == id) {
+                                // console.log(result[i]._id + ' FOUNDED!');
+                                responde = result[i];
+                            }
+                            
+                                // console.log(result[i]._id);
+                        }
+                        // console.log(' <-- End of search --> ');
+                        // console.log('I will send this');
+                        // console.log(responde);
+                        res.send(responde);
+                        res.end();
+                    });
+            client.close();
+        });
+    }
+});
+
+
+app.get('/start_page', (req, res) => {
+    // console.log('Прогрузка каталога....');
+    var client = new MongoClient(url, { useNewUrlParser: true });
+    client.connect(function (err) {
         assert.equal(null, err);
         //console.log("Connected successfully to server");
-      
+
         const db = client.db('myDB');
         db.collection('Store').find(
-            {}, { projection: {_id: 1, name: 1, price: 1, number: 1, img: 1}}).toArray(
+            {}, { projection: { _id: 1, name: 1, price: 1, number: 1, img: 1 } }).toArray(
                 (err, result) => {
-          if (err) throw err;
-          console.log(result);
-          res.send(result);
-          res.end();
-        });
-        
+                    if (err) throw err;
+                    //console.log(result);
+                    res.send(result);
+                    res.end();
+                });
+
         client.close();
-      });
-});
-
-app.get('/:id', function (req, res, next) {
-    console.log('ID:', req.params.id);
-    next();
-  }, function (req, res, next) {
-    res.send('User Info ' + req.params.id);
-});  
-
-app.post('/store',(req,res) => {
-    res.send('POST require');
-});
-
-app.put('/store',(req,res) => {
-    res.send('PUT require');
-});
-
-app.delete('/store',(req,res) => {
-    res.send('DELETE require');
+    });
 });
 
 app.use((req, res, next) => {
@@ -57,11 +84,8 @@ app.use((req, res, next) => {
 });
 
 
-
-
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}...`);
+    // console.log(`Server running on port ${PORT}...`);
 })
