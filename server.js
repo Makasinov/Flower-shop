1f27e3651893143631ca12b3ee63c01b3f6b8e1e
Maksimov <31817995+Makasinov@users.noreply.github.com>
2018-12-07 14:01:34 +0300

    Исправлена ошибка маршрутизации с других устройств

--- server.js
+++ server.js
@@ -46,40 +46,8 @@ app.get('/', async function (req, res) {
     if (sess.email) {
         res.redirect('/admin');
     } else {
-
-        var os = require('os');
-        var ifaces = os.networkInterfaces();
-        var ip = require('ip');
-
-        let myIp;
-        Object.keys(ifaces).forEach(function (ifname) {
-            var alias = 0;
-
-            ifaces[ifname].forEach(function (iface) {
-                if ('IPv4' !== iface.family || iface.internal !== false) {
-                    // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-                    console.log(iface);
-                    return;
-                }
-
-                if (alias >= 1) {
-                    // this single interface has multiple ipv4 addresses
-                    // console.log(ifname + ':' + alias, iface.address);
-                } else {
-                    // this interface has only one ipv4 adress
-                    if (ip.address() == iface.address)
-                        myIp = ip.address();
-                    // console.log('my ip -> ',ip.address(),'\tconnected ip -> ', iface.address);
-                    // console.log(myIp);
-                }
-                ++alias;
-            });
-        });
-
-
         res.render('home', {
-            data: products,
-            adress: myIp
+            data: products
         });
     }
 });
